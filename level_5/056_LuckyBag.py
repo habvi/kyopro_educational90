80f4a1cf0f3339b6bc712a6da0cece265444e3d3
habvi <a2231250601h@yahoo.co.jp>
2022-02-02 01:44:02 +0900

    add: 056

---
+++ level_5/056_LuckyBag.py
@@ -0,0 +1,24 @@
+n, s = map(int, input().split())
+ab = [tuple(map(int, input().split())) for _ in range(n)]
+
+dp = [[''] * (s + 1) for _ in range(n + 1)]
+dp[0][0] = 'S'
+
+for i, (a, b) in enumerate(ab):
+    for j in range(s + 1):
+        if j - a >= 0 and dp[i][j - a]:
+            dp[i + 1][j] = 'A'
+        if j - b >= 0 and dp[i][j - b]:
+            dp[i + 1][j] = 'B'
+
+if not dp[-1][-1]:
+    print('Impossible')
+    exit()
+
+ans = []
+j = s
+for i in reversed(range(1, n + 1)):
+    ans.append(dp[i][j])
+    j -= ab[i - 1]['AB'.index(dp[i][j])]
+    
+print(''.join(ans[::-1]))
