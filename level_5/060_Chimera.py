09dcfe6cad9f0579b495030a5983a3b1fddae7f6
habvi <a2231250601h@yahoo.co.jp>
2022-02-02 16:23:07 +0900

    refactor

--- level_5/060_Chimera.py
+++ level_5/060_Chimera.py
@@ -1,29 +1,29 @@
-from bisect import bisect, bisect_left
+from bisect import bisect_left
 
 n = int(input())
 A = list(map(int, input().split()))
 
 lis_l = [0]
 left = []
+inc = 0
 for a in A:
     if a > lis_l[-1]:
         lis_l.append(a)
-        left.append(len(lis_l) - 1)
+        inc += 1
     else:
-        bi = bisect_left(lis_l, a)
-        lis_l[bi] = a
-        left.append(bi)
+        lis_l[bisect_left(lis_l, a)] = a
+    left.append(inc)
 
 lis_r = [0]
 right = []
+inc = 0
 for a in reversed(A):
     if a > lis_r[-1]:
         lis_r.append(a)
-        right.append(len(lis_r) - 1)
+        inc += 1
     else:
-        bi = bisect_left(lis_r, a)
-        lis_r[bi] = a
-        right.append(bi)
+        lis_r[bisect_left(lis_r, a)] = a
+    right.append(inc)
 right = right[::-1]
 
 ans = 0
