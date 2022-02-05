efc27c838750e3523625fd5495bb1166e253f7a0
habvi <a2231250601h@yahoo.co.jp>
2022-02-05 19:33:01 +0900

    add: n <= 5000

--- level_6/011_GravyJobs.py
+++ level_6/011_GravyJobs.py
@@ -1,3 +1,18 @@
+# n <= 5000
+def solve2():
+    day = 5000
+    dp = [0] * (day + 1)
+
+    for d, c, s in A:
+        ndp = [0] * (day + 1)
+        for i in range(day + 1):
+            ndp[i] = max(ndp[i], dp[i])
+            if i + c <= min(d, day):
+                ndp[i + c] = max(ndp[i + c], dp[i] + s)
+        dp = ndp
+    print(max(dp))
+
+
 # N <= 20
 def solve1():
     ans = 0
@@ -22,4 +37,4 @@ A.sort(key=lambda x: x[0])
 if n <= 20:
     solve1()
 else:
-    exit()
+    solve2()
